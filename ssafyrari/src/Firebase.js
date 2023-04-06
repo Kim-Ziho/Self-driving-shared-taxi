185fb9a0fe00188066412a135cd2ce62aa2a13b2
박상현 <pas4584@naver.com>
2023-04-06 15:26:28 +0900

    firebase config

--- ssafyrari/src/Firebase.js
+++ ssafyrari/src/Firebase.js
@@ -8,24 +8,24 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA2cpVK_z80ndQtR81qrq1-2XZ4wiWsFBE",
-//   authDomain: "ssafyrari.firebaseapp.com",
-//   projectId: "ssafyrari",
-//   storageBucket: "ssafyrari.appspot.com",
-//   messagingSenderId: "841921736383",
-//   appId: "1:841921736383:web:e71afde3acff306dd0e00a",
-//   measurementId: "G-CDQGCSDBMY",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDgvpCxqBJkr7Mqm6yIsLSz_sqsL9xp4IU",
-  authDomain: "ssafyrari-a5ba6.firebaseapp.com",
-  projectId: "ssafyrari-a5ba6",
-  storageBucket: "ssafyrari-a5ba6.appspot.com",
-  messagingSenderId: "695297315640",
-  appId: "1:695297315640:web:fb5f9787ad37df726fe7cd",
+  apiKey: "AIzaSyA2cpVK_z80ndQtR81qrq1-2XZ4wiWsFBE",
+  authDomain: "ssafyrari.firebaseapp.com",
+  projectId: "ssafyrari",
+  storageBucket: "ssafyrari.appspot.com",
+  messagingSenderId: "841921736383",
+  appId: "1:841921736383:web:e71afde3acff306dd0e00a",
+  measurementId: "G-CDQGCSDBMY",
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDgvpCxqBJkr7Mqm6yIsLSz_sqsL9xp4IU",
+//   authDomain: "ssafyrari-a5ba6.firebaseapp.com",
+//   projectId: "ssafyrari-a5ba6",
+//   storageBucket: "ssafyrari-a5ba6.appspot.com",
+//   messagingSenderId: "695297315640",
+//   appId: "1:695297315640:web:fb5f9787ad37df726fe7cd",
+// };
 
 // const firebaseConfig = {
 //   apiKey: "AIzaSyCfjuE2YywtBQal0LossV-ZZptPOKRFc7Y",
